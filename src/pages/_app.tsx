6b7ab8c9d608ab4f6fcc086fa628c20d73cbf377
thititongumpun <thiti180536@gmail.com>
2023-09-13 13:36:12 +0700

    chore: update stripe api version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -71,7 +71,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
                   console.error("Script failed to load", e);
                 }}
                 src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID as string}`}
-                strategy="lazyOnload"
+                strategy="worker"
                 crossOrigin="anonymous"
               />
               <Script
